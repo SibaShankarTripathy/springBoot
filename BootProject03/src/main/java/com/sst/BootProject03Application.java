bd72c36f75c7804f5a41ad012d9a4dfc845c53a2
SibaShankar <sibashankartripathy4@gmail.com>
2024-03-11 23:17:51 +0530

    project 3 commit not completed

---
+++ BootProject03/src/main/java/com/sst/BootProject03Application.java
@@ -0,0 +1,13 @@
+package com.sst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProject03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProject03Application.class, args);
+	}
+
+}
---
+++ BootProject03/src/main/java/com/sst/store/GeneralStore.java
@@ -0,0 +1,21 @@
+package com.sst.store;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class GeneralStore {
+/*
+ In this class we will see how we can inject value in an Array, Collection(List,
+ Set, Map),HAS-A variable.
+ */
+
+//Normal variable
+private String snacks;
+private String softDrink;
+
+//Array type
+private String[] chocolate;
+
+	
+	
+}
